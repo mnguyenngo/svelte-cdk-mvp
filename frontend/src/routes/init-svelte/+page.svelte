e109261e333e77ecf7b09fdba7bb047e0c161293
Nguyen Ngo <mnguyenngo@gmail.com>
2024-11-11 15:40:48 -0800

    1 initialize svelte project

---
+++ frontend/src/routes/init-svelte/+page.svelte
@@ -0,0 +1,38 @@
+<div><a href="/">back home</a></div>
+
+<hr>
+
+<div>
+  <h1> Initializing the Svelte Project</h1>
+  <p>created on 11/11/2024</p>
+</div>
+
+<hr>
+
+<div>
+  <p>
+    I created the svelte project following these <a href="https://svelte.dev/docs/kit/creating-a-project">docs</a>.
+  </p>
+
+  <p>Integrations that I included at setup time:</p>
+  <ul>
+    <li>eslint</li>
+    <li>prettier</li>
+    <li>tailwindcss</li>
+    <li><a href="https://mdsvex.pngwn.io/">mdsvex</a> (I plan on writing these posts in markdown in the future.)</li>
+  </ul>
+
+  <p>
+    I expect that I will use these integrations the most. See other available integrations <a href="https://svelte.dev/docs/kit/integrations">here</a>.
+  </p>
+
+</div>
+
+
+<div>
+  <h2>References</h2>
+
+  <ul>
+    <li><a href="https://svelte.dev/docs/kit">SvelteKit Docs</a></li>
+  </ul>
+</div>
